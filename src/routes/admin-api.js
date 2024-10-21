998e143a0e78bc4d1497e7e45e76804fe95e6b61
atleastt <rizkywaom@gmail.com>
2024-10-21 10:43:33 +0700

    fixing router

--- src/routes/admin-api.js
+++ src/routes/admin-api.js
@@ -13,17 +13,17 @@ adminRouter.get('/api/admin/users', userController.get);
 adminRouter.get('/api/admin/users/get-one', userController.getUserByUsername);
 adminRouter.patch('/api/admin/user/update', userController.updateUserByUsername);
 
-userRouter.post('/api/uom/create', uomController.createUOM);
-userRouter.put('/api/uom/update/:id', uomController.updateUOM);
-userRouter.delete('/api/uom/delete/:id', uomController.deleteUOM);
+adminRouter.post('/api/uom/create', uomController.createUOM);
+adminRouter.put('/api/uom/update/:id', uomController.updateUOM);
+adminRouter.delete('/api/uom/delete/:id', uomController.deleteUOM);
 
 adminRouter.post('/api/pricelist', pricelistController.postPricelist);
 adminRouter.patch('/api/pricelist/update', pricelistController.updatePricelist);
 adminRouter.delete('/api/pricelist/delete/:wasteId/:uomId', pricelistController.deletePricelist);
 
-userRouter.post('/api/waste-category/create', wasteCategoryController.createWasteCategory);
-userRouter.put('/api/waste-category/update/:id', wasteCategoryController.updateWasteCategory);
-userRouter.delete('/api/waste-category/delete/:id', wasteCategoryController.deleteWasteCategory);
+adminRouter.post('/api/waste-category/create', wasteCategoryController.createWasteCategory);
+adminRouter.put('/api/waste-category/update/:id', wasteCategoryController.updateWasteCategory);
+adminRouter.delete('/api/waste-category/delete/:id', wasteCategoryController.deleteWasteCategory);
 
 export {
     adminRouter
